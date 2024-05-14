50805fe35c5bb6128d29d6663f65f784e8b94445
Oleksandr Moroz <molecxander@gmail.com>
2024-05-14 18:23:13 +0300

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -89,7 +89,7 @@ class DriverListView(LoginRequiredMixin, generic.ListView):
 
 class DriverDetailView(LoginRequiredMixin, generic.DetailView):
     model = Driver
-    queryset = Driver.objects.all().prefetch_related("cars__manufacturer")
+    queryset = Driver.objects.prefetch_related("cars__manufacturer")
 
 
 class DriverCreate(LoginRequiredMixin, generic.CreateView):
