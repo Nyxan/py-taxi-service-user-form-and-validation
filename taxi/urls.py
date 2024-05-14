e08a8b4365e917d023a1e1796f3450c5cbb8af2d
Oleksandr Moroz <molecxander@gmail.com>
2024-05-14 12:08:16 +0300

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -59,7 +59,7 @@ urlpatterns = [
     path("drivers/<int:pk>/delete/",
          DriverDelete.as_view(),
          name="driver-delete"),
-    path("car/<int:car_id>/add-driver/", add_and_remove_driver, name="add-driver"),
+    path("car/<int:car_id>/add-driver/", add_and_remove_driver, name="add-remove-driver"),
 ]
 
 app_name = "taxi"
